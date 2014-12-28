ac6dca4fd2880023bc657b294e60649ee13fc03c
toddhollen <toddhollen@gmail.com>
2014-12-27 18:31:11 -0700

    Static Page

--- js/app.js
+++ js/app.js
@@ -1,5 +1,13 @@
 $(document).ready(function($) {
-	
 
+ function initialize() {
+        var mapOptions = {
+          center: { lat: 38.833882, lng: -104.821363},
+          zoom: 3
+        };
+        var map = new google.maps.Map(document.getElementById('map-canvas'),
+            mapOptions);
+      }
+      google.maps.event.addDomListener(window, 'load', initialize);
 	
 });
