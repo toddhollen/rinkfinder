886a36537ba0b638eee3332ef7d645b9b7c8d1cf
toddhollen <toddhollen@gmail.com>
2014-12-29 17:56:31 -0700

    Added JS

--- js/app.js
+++ js/app.js
@@ -1,13 +1,38 @@
 $(document).ready(function($) {
 
  function initialize() {
-        var mapOptions = {
-          center: { lat: 38.833882, lng: -104.821363},
-          zoom: 3
-        };
-        var map = new google.maps.Map(document.getElementById('map-canvas'),
-            mapOptions);
+
+ // Loads generic map on page ready
+
+ 	var myLatlng = new google.maps.LatLng(38.833882, -104.821363);
+	var mapOptions = {
+		  zoom: 4,
+		  center: myLatlng,
+		  mapTypeId: google.maps.MapTypeId.HYBRID
+	};
+
+	var map = new google.maps.Map(document.getElementById("map-canvas"),
+    mapOptions);
+
       }
       google.maps.event.addDomListener(window, 'load', initialize);
-	
+
+
 });
+
+
+// Collect user submitted zipcode
+
+$('.form').submit( function(event){
+
+  		// get the value of the zipcode user submitted to zip form
+		var location = $(this).find("input[name='zip']").val();
+
+
+		console.log(location);
+
+	});
+
+
+
+	
