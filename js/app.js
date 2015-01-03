b4814fafa0ba31fc8a2152d62f21aa183d135445
toddhollen <toddhollen@gmail.com>
2015-01-02 17:37:45 -0700

    Copied Sample code from Google example

--- js/app.js
+++ js/app.js
@@ -1,23 +1,42 @@
 $(document).ready(function($) {
 
- function initialize() {
+ var map;
+var service;
 
- // Loads generic map on page ready
+function initialize() {
+  map = new google.maps.Map(document.getElementById('map-canvas'), {
+    center: new google.maps.LatLng(80903),
+    zoom: 15
+  });
 
- 	var myLatlng = new google.maps.LatLng(38.833882, -104.821363);
-	var mapOptions = {
-		  zoom: 4,
-		  center: myLatlng,
-		  mapTypeId: google.maps.MapTypeId.HYBRID
-	};
+  infoWindow = new google.maps.InfoWindow();
+  service = new google.maps.places.PlacesService(map);
 
-	var map = new google.maps.Map(document.getElementById("map-canvas"),
-    mapOptions);
+  google.maps.event.addListenerOnce(map, 'bounds_changed', performSearch);
+}
 
-      }
+function performSearch() {
+  var request = {
+    bounds: map.getBounds(),
+    keyword: 'ice rinks'
+  };
+  service.radarSearch(request, callback);
+}
 
-      google.maps.event.addDomListener(window, 'load', initialize);
+function callback(results, status) {
+  if (status != google.maps.places.PlacesServiceStatus.OK) {
+    alert(status);
+    return;
+  }
+  for (var i = 0, result; result = results[i]; i++) {
+    var marker = new google.maps.Marker({
+      map: map,
+      position: result.geometry.location
+    });
+  }
+}
 
+google.maps.event.addDomListener(window, 'load', initialize);
 });
 
 
