708e596fd72ae309a44d51cd14b81229f407f52b
toddhollen <toddhollen@gmail.com>
2015-01-09 16:01:47 -0700

    Updates

--- js/app.js
+++ js/app.js
@@ -1,56 +1,87 @@
 $(document).ready(function($) {
 
-var map;
-var service;
+  var map;
+  var service;
+  var infoWindow;
 
-function initialize() {
-  map = new google.maps.Map(document.getElementById('map-canvas'), {
-    center: new google.maps.LatLng(38.833882, -104.821363),
-    zoom: 5
-  });
+  function initialize() {
+    mapCenter = new google.maps.LatLng(38.833882, -104.821363);
+    
+    map = new google.maps.Map(document.getElementById('map-canvas'), {
+      center: mapCenter,
+      zoom: 5
+    });
+    
+    infoWindow = new google.maps.InfoWindow();
+    
+    service = new google.maps.places.PlacesService(map);
 
-  infoWindow = new google.maps.InfoWindow();
-  service = new google.maps.places.PlacesService(map);
-
-  google.maps.event.addListenerOnce(map, 'bounds_changed', performSearch);
-}
-
-function performSearch() {
-  var request = {
-    bounds: map.getBounds(),
-    query: 'ice rinks'
-  };
-  service.radarSearch(request, callback);
-}
-
-function callback(results, status) {
-  if (status != google.maps.places.PlacesServiceStatus.OK) {
-    alert(status);
-    return;
+    // google.maps.event.addListenerOnce(map, 'bounds_changed', performSearch);
+    performSearch();
   }
-  for (var i = 0, result; result = results[i]; i++) {
+
+  function performSearch() {
+    var request = {
+      location: mapCenter,
+      radius: '50',
+      query: 'ice rink'
+    };
+    
+    service.textSearch(request, callback);
+  }
+
+  function callback(results, status) {
+    if (status == google.maps.places.PlacesServiceStatus.OK) {
+      for (var i = 0; i < results.length; i++) {
+        var place = results[i];
+        createMarker(results[i]);
+      }
+    }
+  }
+
+  function createMarker(place) {
+    var placeLoc = place.geometry.location;
     var marker = new google.maps.Marker({
       map: map,
-      position: result.geometry.location
+      position: place.geometry.location
+    });
+
+    google.maps.event.addListener(marker, 'click', function() {
+      infoWindow.setContent(place.name);
+      infoWindow.open(map, this);
     });
   }
-}
 
-google.maps.event.addDomListener(window, 'load', initialize);
-});
-
-
-// Collect user submitted zipcode
-
-$('.form').submit( function(event){
-
-  		// get the value of the zipcode user submitted to zip form
-		var location = $(this).find("input[name='zip']").val();
-
-		console.log(location);
-
-});
+  google.maps.event.addDomListener(window, 'load', initialize);
 
 
 
-	
+
+  // Cancel defualt form submit
+  $('form').on('submit', function(event){ 
+    event.preventDefault(); 
+  });
+
+  // Collect user submitted zipcode
+  $('input[type="submit"]').on( "click" , function(event){
+      // Cancel initial submit
+      event.preventDefault();
+
+      // get the value of the zipcode user submitted to zip form
+      var mapNewCenter = $('form').find("input[name='zip']").val();
+
+  function performSearch() {
+    var request = {
+      location: mapNewCenter,
+      radius: '50',
+      query: 'ice rink'
+    };
+    
+    service.textSearch(request, callback);
+  }
+
+
+  });
+
+
+}); // End of document.ready
